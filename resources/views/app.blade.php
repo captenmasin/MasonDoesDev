d09081a87954c95575650240654703a74f958b68
Mason Day <daymason@hotmail.co.uk>
2020-12-10 12:58:13 +0000

    OG Tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="{{ asset('css/app.css') }}?v={{ time() }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <meta property="og:url" content="{{ request()->url() }}"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="{{ config('app.name') }}"/>
+    <meta property="og:description" content="A random guy who makes things"/>
+    <meta property="og:image" content="{{ config('app.url') }}/img/lou-and-mas.jpg"/>
 </head>
 <body class="bg-green-800">
 @inertia
