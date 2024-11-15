c1cef89a6f2997ce1d4841184779eea0df339a53
Mason Day <daymason@hotmail.co.uk>
2024-11-15 17:14:26 +0000

    Added contact page

---
+++ contact.php
@@ -0,0 +1,75 @@
+<?php
+$description = 'Full-stack developer with a couple fun side-projects';
+
+$websites = [
+    'novogamer.com'    => 'https://icons.duckduckgo.com/ip3/novogamer.com.ico',
+    'annoyingbeep.com' => 'https://icons.duckduckgo.com/ip3/annoyingbeep.com.ico',
+    'seotoolkit.dev'   => 'https://icons.duckduckgo.com/ip3/seotoolkit.dev.ico',
+    'browser.is'       => 'https://icons.duckduckgo.com/ip3/browser.is.ico',
+];
+?>
+
+<!doctype html>
+<html class="no-js h-full flex flex-1" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>mason - contact</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+
+    <script defer src="https://api.pirsch.io/pa.js" id="pianjs" data-code="eS7tDC6VGNOnoaRSEiR8RsgGFgHpfvkk"></script>
+
+    <meta name="description" content="<?= $description ?>">
+    <meta property="og:title" content="masondoes.dev">
+    <meta property="og:url" content="https://masondoes.dev">
+    <meta property="og:image" content="https://masondoes.dev/img/og.png">
+    <meta property="og:description" content="<?= $description ?>">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:creator" content="@capten_masin">
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <meta name="theme-color" content="#E7E5E4">
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Mason Day",
+            "alternateName": "capten_masin",
+            "jobTitle": "Web Developer",
+            "url": "https://masondoes.dev",
+            "sameAs": [
+                "https://x.com/capten_masin",
+                "https://github.com/captenmasin"
+            ],
+            "image": "https://masondoes.dev/img/og.png",
+            "description": "<?= $description ?>",
+            "knowsAbout": [
+                "Web Development",
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "Vue",
+                "PHP",
+                "Laravel"
+            ],
+        }
+    </script>
+</head>
+
+<body class="h-full flex flex-1 bg-gradient-to-b from-[#FAFAF9] to-[#E7E5E4]">
+<div class="flex flex-col gap-1 items-center justify-center w-full h-full">
+    <h1 class="font-heading leading-0 -mt-8 text-primary text-9xl">
+        how to contact me
+    </h1>
+    <h2 class="font-heading text-5xl">
+        don't
+    </h2>
+</div>
+</body>
+
+</html>
