ad178293aeb523d0818ad0fffb05bab3e9ad8169
Mason Day <daymason@hotmail.co.uk>
2026-04-16 20:15:51 +0100

    Dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	fonts: [{
+		provider: fontProviders.fontsource(),
+		name: "Playfair Display",
+		cssVariable: "--font-playfair",
+	}],
   vite: {
     plugins: [tailwindcss()]
   }
