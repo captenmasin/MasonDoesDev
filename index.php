01bfa0315cb30e726194b5f5bbdaf8df3576d401
Mason Day <daymason@hotmail.co.uk>
2026-04-16 17:06:24 +0100

    Rebuild with Astro

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$currentRoute = str_replace("/", "", $_SERVER['REQUEST_URI']);
-
-if ($currentRoute === 'contact') {
-    require_once 'contact.php';
-} else{
-    require_once 'home.php';
-}
-?>
